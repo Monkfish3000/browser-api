2a5da663f6d543023c826adb36ae663c32374c5d
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-03-15 19:52:19 +0100

    DOM exploration with composedPath()

--- examples/events/index.js
+++ examples/events/index.js
@@ -1,4 +1,17 @@
 const clickMeBtn = document.getElementById('click_me');
 const clickValue = document.getElementById('click_value');
+const changeMe = document.getElementById('change_me');
+const card = document.querySelector('.card');
 
 clickMeBtn.addEventListener('click', () => clickValue.value++);
+
+changeMe.addEventListener('blur', () =>
+  console.log('on blur ' + changeMe.value)
+);
+changeMe.addEventListener('input', (event) =>
+  console.log('any input change ' + event.target.value)
+);
+
+card.addEventListener('click', (event) => {
+  console.log(event.composedPath());
+});
